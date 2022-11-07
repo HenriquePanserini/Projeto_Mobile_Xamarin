0ea1dd326e94560829c696958ac9d1d251037adc
Henrique Panserini <henrique.panserini@gmail.com>
2022-11-07 17:07:21 -0300

    management application(Xamarin)-V0.2

--- AppTol/AppTol/Master.xaml.cs
+++ AppTol/AppTol/Master.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Android.OS;
+using DocumentFormat.OpenXml.Drawing;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,46 @@ namespace AppTol
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Master : ContentPage
     {
+        public Button buttonUser;
+        public Button buttonSincro;
+        public Button buttonCad;
+        public Button buttonLimp;
+        public Button buttonSair;
+
+        Master master = new Master();
         public Master()
         {
-            InitializeComponent();
+            InitializeComponent();  
         }
+
+        protected void Create()
+        {
+            buttonSair = master.FindByName<Button>("btn_sair");
+            buttonSair.Clicked += ButtonSair_Clicked;
+
+            buttonSincro = master.FindByName<Button>("btn_sinc");
+            buttonSincro.Clicked += ButtonSincro_Clicked;
+        }
+
+     
+  
+        private void ButtonSincro_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void ButtonSair_Clicked(object sender, EventArgs e)
+        {
+            bool resposta = await DisplayAlert("Aviso!", "Deseja mesmo encerrar o aplicativo", "Yes", "No");
+
+            if (resposta = false)
+            {
+
+                System.Environment.Exit(0);
+
+            }
+            
+        }
+
     }
 }
