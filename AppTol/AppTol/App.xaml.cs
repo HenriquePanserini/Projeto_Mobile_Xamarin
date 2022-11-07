2b286c580eb785fba58f1a0818b8be584a4ac29d
Henrique Panserini <henrique.panserini@gmail.com>
2022-11-07 09:05:57 -0300

    management application(Xamarin)-V0.1

---
+++ AppTol/AppTol/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTol
+{
+    
+    public partial class App : Application
+    {
+
+        public static MasterDetailPage masterDetail { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
